ed0f03f4c9bab24f34ed2d2e66461ff050926a15
Watch-dog-s <126652678+Watch-dog-s@users.noreply.github.com>
2024-03-20 11:11:05 +0300

    add django project. Links in HTML does not work. CSS does not work

---
+++ myproject/main/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.index),
+    path('user_home', views.about),
+
+    path('page2', views.page2),
+    path('page3', views.page3),
+    path('page4', views.page4)
+
+
+]
---
+++ myproject/main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,'main/test.html')
+
+def about(request):
+    return render(request,'main/user_home.html')
+
+
+def page2(request):
+    return render(request,'main/page2.html')
+
+
+def page3(request):
+    return render(request,'main/page3.html')
+
+
+def page4(request):
+    return render(request,'main/page4.html')
